0c6d45180b2ac2aaae3bd3c7965d52ea3d7e24d6
Chris Milson <chris.w.milson@gmail.com>
2020-03-12 09:10:45 +0900

    Add subtraction version of fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -3,15 +3,31 @@ from coinflip import coinflip
 class Original:
   """The original fibonacci sequence."""
   def __init__(self):
-    self.memo = [1, 1]
+    self._memo = [1, 1]
 
   def __getitem__(self, index):
-    current = len(self.memo)
+    current = len(self._memo)
     while current <= index:
-      self.memo.append(self.memo[current - 1] + self.memo[current - 2])
+      self._memo.append(self._memo[current - 1] + self._memo[current - 2])
       current += 1
     
-    return self.memo[index]
+    return self._memo[index]
+
+class Subtract:
+  """A sequence similar to fibonacci, but where the previous values are
+  subtracted instead of added.
+  """
+  def __init__(self):
+    self._memo = [1, 1]
+
+  def __getitem__(self, index):
+    current = len(self._memo)
+
+    while current <= index:
+      self._memo.append(self._memo[current - 1] - self._memo[current - 2])
+      current += 1
+
+    return self._memo[index]
 
 class Random:
   """A random sequence based around a fibonacci style recurrence relation."""
