fb0cecd93fa88c5d650a256d59eda2964fd59929
Chris Milson <chris.w.milson@gmail.com>
2020-03-12 09:52:20 +0900

    Reduce code repetition
    By defining a recurrence class that takes a relationship and making the
    individual sequences inherit from this class, we can reduce code
    repetition and only have to define the relationship for a given
    sequence. The Recurrence class can take care of the memoisation
    algorithm.

--- fibonacci.py
+++ fibonacci.py
@@ -1,49 +1,52 @@
 from coinflip import coinflip
 
-class Original:
-  """The original fibonacci sequence."""
-  def __init__(self):
-    self._memo = [1, 1]
+class Recurrence:
+  """A recurrence relation based sequence
+  
+  Args:
+    relation (int[], int -> int): A function that can be used to calculate the 
+      next value in the sequence given the previous values.
+    initial (int[]): An array representing the initial state of the sequence;
+      the relation should return the next value in the sequence given this
+      array.
+  """
+  def __init__(self, relation, initial):
+    self._memo = initial
+
+    def next(target):
+      """calculates the next value in the sequence"""
+      return relation(self._memo, target)
+
+    self._next = next
 
   def __getitem__(self, index):
-    current = len(self._memo)
-    while current <= index:
-      self._memo.append(self._memo[current - 1] + self._memo[current - 2])
-      current += 1
-    
+    for target in range(len(self._memo), index + 1):
+      self._memo.append(self._next(target))
+
     return self._memo[index]
 
-class Subtract:
+class Original(Recurrence):
+  """The original fibonacci sequence."""
+  def __init__(self):
+    def relation(seq, target):
+      return seq[target - 1] + seq[target - 2]
+
+    super().__init__(relation, [1, 1])
+
+class Subtract(Recurrence):
   """A sequence similar to fibonacci, but where the previous values are
   subtracted instead of added.
   """
   def __init__(self):
-    self._memo = [1, 1]
+    def relation(seq, target):
+      return seq[target - 1] - seq[target - 2]
 
-  def __getitem__(self, index):
-    current = len(self._memo)
+    super().__init__(relation, [1, 1])
 
-    while current <= index:
-      self._memo.append(self._memo[current - 1] - self._memo[current - 2])
-      current += 1
-
-    return self._memo[index]
-
-class Random:
+class Random(Recurrence):
   """A random sequence based around a fibonacci style recurrence relation."""
-
   def __init__(self):
-    self._memo = [1, 1]
+    def relation(seq, target):
+      return seq[target - 1] + (1 if coinflip() else -1) * seq[target - 2]
 
-  def __getitem__(self, index):
-    current = len(self._memo)
-
-    while current <= index:
-      self._memo.append(
-        self._memo[current - 1] +
-        (1 if coinflip() else -1) *
-        self._memo[current - 2]
-      )
-      current += 1
-    
-    return self._memo[index]
+    super().__init__(relation, [1, 1])
