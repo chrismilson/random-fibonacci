f2a7616d966ac1c734fcff287bd970a8d61ab3b9
Chris Milson <chris.w.milson@gmail.com>
2020-03-12 15:25:09 +0900

    Add printing method
    The recurrence relations now have a buildt in printing method so that
    the notebook can be cleaner.

--- recurrence.py
+++ recurrence.py
@@ -7,9 +7,11 @@ class Relation:
     initial (int[]): An array representing the initial state of the sequence;
       the relation should return the next value in the sequence given this
       array.
+    name (str): A human readable name representing the sequence.
   """
-  def __init__(self, relation, initial):
+  def __init__(self, relation, initial, name=''):
     self._memo = initial
+    self.name = name
 
     def next(target):
       """calculates the next value in the sequence"""
@@ -22,3 +24,17 @@ class Relation:
       self._memo.append(self._next(target))
 
     return self._memo[index]
+
+  def printFirst(self, n):
+    print(
+      'The first {} numbers'.format(n),
+      ('of {}'.format(self.name) if self.name else ''),
+      'are:'
+    )
+
+    delimeter = ', ' if n < 30 else ',\n'
+
+    for i in range(n - 1):
+      print(self[i], end=delimeter)
+
+    print('and', self[n - 1], end='.\n')
